25ebb38b3302937c7f56eaf5f1054517aef6b51a
NamousNassim <nassimnamousdev@gmail.com>
2024-09-04 23:02:32 +0100

    init

---
+++ src/assets/data/carData.js
@@ -0,0 +1,142 @@
+// import all images from assets/images directory
+import img01 from "../all-images/cars-img/ClioVAuto.png";
+import img02 from "../all-images/cars-img/ClioVMann.png";
+import img03 from "../all-images/cars-img/bmw-offer.png";
+import img04 from "../all-images/cars-img/nissan-offer.png";
+import img05 from "../all-images/cars-img/offer-toyota.png";
+import img06 from "../all-images/cars-img/mercedes-offer.png";
+import img07 from "../all-images/cars-img/toyota-offer-2.png";
+import img08 from "../all-images/cars-img/mercedes-offer.png";
+
+const carData = [
+  {
+    id: 1,
+    brand: "Renault ",
+    rating: 112,
+    carName: "Renault Clio 5",
+    imgUrl: img01,
+    model: "2023",
+    price: 350,
+    energie: " Essence",
+     
+    automatic: "Automatic",
+    description:
+      "La Renault Clio 5 de 2023 est une citadine élégante et compacte qui allie design moderne et technologie avancée. Réputée pour ses lignes dynamiques et son profil audacieux, la Clio 5 offre un confort de conduite remarquable avec une excellente efficacité énergétique, idéale tant pour la conduite en ville que pour les longs trajets. La transmission automatique garantit une expérience de conduite fluide, tandis que sa taille compacte assure une grande agilité en milieu urbain. À l'intérieur, la Clio 5 est équipée du dernier système d'infodivertissement et de sièges ergonomiques, offrant à la fois confort et praticité. Que vous soyez en déplacement quotidien ou en exploration, la Renault Clio 5 vous propose une expérience de conduite raffinée à un prix abordable ",
+
+  
+  },
+
+  {
+    id: 2,
+    brand: "Renault ",
+    rating: 112,
+    carName: "Renault Clio 5",
+    imgUrl: img02,
+    model: "2023",
+    price: 350,
+    energie: "Diesel",
+      
+    automatic: "Manuel",
+    description:
+      "La Renault Clio 5 de 2023 est une citadine élégante et compacte qui allie design moderne et technologie avancée. Réputée pour ses lignes dynamiques et son profil audacieux, la Clio 5 offre un confort de conduite remarquable avec une excellente efficacité énergétique, idéale tant pour la conduite en ville que pour les longs trajets. La transmission automatique garantit une expérience de conduite fluide, tandis que sa taille compacte assure une grande agilité en milieu urbain. À l'intérieur, la Clio 5 est équipée du dernier système d'infodivertissement et de sièges ergonomiques, offrant à la fois confort et praticité. Que vous soyez en déplacement quotidien ou en exploration, la Renault Clio 5 vous propose une expérience de conduite raffinée à un prix abordable ",
+
+  },
+
+  {
+    id: 3,
+    brand: "BMW",
+    rating: 132,
+    carName: "BMW X3",
+    imgUrl: img03,
+    model: "Model-2022",
+    price: 65,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 4,
+    brand: "Nissan",
+    rating: 102,
+    carName: "Nissan Mercielago",
+    imgUrl: img04,
+    model: "Model-2022",
+    price: 70,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 5,
+    brand: "Ferrari",
+    rating: 94,
+    carName: "Ferrari Camry",
+    imgUrl: img05,
+    model: "Model-2022",
+    price: 45,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 6,
+    brand: "Mercedes",
+    rating: 119,
+    carName: "Mercedes Benz XC90",
+    imgUrl: img06,
+    model: "Model-2022",
+    price: 85,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 7,
+    brand: "Audi",
+    rating: 82,
+    carName: "Audi Fiesta",
+    imgUrl: img07,
+    model: "Model 3",
+    price: 50,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 8,
+    brand: "Colorado",
+    rating: 52,
+    carName: "Rolls Royce Colorado",
+    imgUrl: img08,
+    model: "Model 3",
+    price: 50,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+];
+
+export default carData;
