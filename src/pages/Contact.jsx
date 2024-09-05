f60c03ff11cc593fca523e5206d90de3711605f5
NamousNassim <nassimnamousdev@gmail.com>
2024-09-05 23:07:20 +0100

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -33,11 +33,11 @@ const Contact = () => {
         <Container>
           <Row>
             <Col lg="7" md="7">
-              <h6 className="fw-bold mb-4">Get In Touch</h6>
+              <h6 className="fw-bold mb-4">Entrer en Contact</h6>
 
               <Form>
                 <FormGroup className="contact__form">
-                  <Input placeholder="Your Name" type="text" />
+                  <Input placeholder="Votre Nom" type="text" />
                 </FormGroup>
                 <FormGroup className="contact__form">
                   <Input placeholder="Email" type="email" />
@@ -51,28 +51,28 @@ const Contact = () => {
                 </FormGroup>
 
                 <button className=" contact__btn" type="submit">
-                  Send Message
+                  Envoyer le Message
                 </button>
               </Form>
             </Col>
 
             <Col lg="5" md="5">
               <div className="contact__info">
-                <h6 className="fw-bold">Contact Information</h6>
+                <h6 className="fw-bold">Informations de Contact</h6>
                 <p className="section__description mb-0">
-                  123 ZindaBazar, Sylhet, Bangladesh
+                  123 Rue de la Kasbah, Marrakech, Maroc
                 </p>
                 <div className=" d-flex align-items-center gap-2">
-                  <h6 className="fs-6 mb-0">Phone:</h6>
-                  <p className="section__description mb-0">+88683896366</p>
+                  <h6 className="fs-6 mb-0">Téléphone:</h6>
+                  <p className="section__description mb-0">+212612345678</p>
                 </div>
 
                 <div className=" d-flex align-items-center gap-2">
                   <h6 className="mb-0 fs-6">Email:</h6>
-                  <p className="section__description mb-0">example@gmail.com</p>
+                  <p className="section__description mb-0">exemple@gmail.com</p>
                 </div>
 
-                <h6 className="fw-bold mt-4">Follow Us</h6>
+                <h6 className="fw-bold mt-4">Suivez-nous</h6>
 
                 <div className=" d-flex align-items-center gap-4 mt-3">
                   {socialLinks.map((item, index) => (
@@ -81,7 +81,7 @@ const Contact = () => {
                       key={index}
                       className="social__link-icon"
                     >
-                      <i class={item.icon}></i>
+                      <i className={item.icon}></i>
                     </Link>
                   ))}
                 </div>
